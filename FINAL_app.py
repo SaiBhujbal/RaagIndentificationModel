ea9efb9c1f61b074e7d7128539e316a87ff54b52
Slayer <46700402+SaiBhujbal@users.noreply.github.com>
2023-10-30 05:05:22 +0530

    Update FINAL_app.py

--- FINAL_app.py
+++ FINAL_app.py
@@ -1,55 +1,33 @@
 import streamlit as st
 import librosa
 import numpy as np
-from pydub import AudioSegment
-from io import BytesIO
-from scipy import signal
-from tensorflow.keras.models import load_model
-import joblib
+from tensorflow import keras
+import pickle
 
 # Load the trained model
-model = load_model('mfcc_cnn_model_2.h5')  # Load the saved model
+model = keras.models.load_model('mfcc_cnn_model_2.h5')  # Load the saved model
 
-# Load the label encoder from the pickled file
-label_encoder = joblib.load('label_encoder.pkl')  # Replace 'label_encoder.pkl' with your label encoder filename
-
-# Function to extract MFCC features from an audio file
-def extract_mfcc(audio, sample_rate, num_mfcc=13, n_fft=2048, hop_length=512):
+# Function to extract MFCC features from an audio signal
+def extract_mfcc(y, sr, num_mfcc=13, n_fft=2048, hop_length=512):
     # Extract MFCC features
-    mfccs = librosa.feature.mfcc(y=audio, sr=sample_rate, n_mfcc=num_mfcc, n_fft=n_fft, hop_length=hop_length)
+    mfccs = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=num_mfcc, n_fft=n_fft, hop_length=hop_length)
 
     # Transpose the MFCC matrix to have shape (number_of_mfcc_coefficients, number_of_frames)
     mfccs = mfccs.T
 
     return mfccs
 
-# Function to detect the most dominant frequency
-def detect_dominant_frequency(audio):
-    sample_rate = audio.frame_rate
-    y = np.array(audio.get_array_of_samples())
-    f, t, S = signal.spectrogram(y, sample_rate)
+# Load labels from the 'labels.pkl' file
+with open('label_encoder.pkl', 'rb') as file:
+    labels = pickle.load(file)
 
-    max_freq_indices = np.argmax(S, axis=0)
-    dominant_freqs = f[max_freq_indices]
+# Function to predict raag from MFCC features
+def predict_raag(audio, model, labels):
+    # Use librosa to load the audio
+    y, sr = librosa.load(audio, sr=None)
 
-    return np.mean(dominant_freqs)
-
-# Function to preprocess and predict raag from MFCC features
-def predict_raag(audio_file, model, label_encoder):
-    audio = AudioSegment.from_file(audio_file, format=audio_file.name.split('.')[-1])
-
-    # Convert the audio to MP3 format in memory
-    audio_bytes = BytesIO()
-    audio.export(audio_bytes, format="mp3")
-    audio_bytes.seek(0)
-    converted_audio = AudioSegment.from_file(audio_bytes, format="mp3")
-
-    # Extract the audio as an array
-    audio_array = np.array(converted_audio.get_array_of_samples())
-    sample_rate = converted_audio.frame_rate
-
-    # Extract MFCC features from the converted audio
-    mfccs = extract_mfcc(audio_array, sample_rate)
+    # Extract MFCC features from the audio signal
+    mfccs = extract_mfcc(y, sr)
 
     # Reshape and normalize the MFCC data
     desired_shape = (216, 13)  # Shape expected by the model
@@ -65,13 +43,13 @@ def predict_raag(audio_file, model, label_encoder):
     # Make predictions using the loaded model
     predictions = model.predict(mfccs)
 
-    # Get the predicted label
-    predicted_label = np.argmax(predictions, axis=1)
-    
-    # Map the predicted label to raag names
-    predicted_raag = label_encoder.inverse_transform(predicted_label)
+    # Get the predicted label index
+    predicted_index = np.argmax(predictions, axis=1)
 
-    return predicted_raag
+    # Get the predicted label from the labels list
+    predicted_label = labels[predicted_index[0]]  # Assuming 'labels' is a list of labels corresponding to indices
+
+    return predicted_label
 
 # Streamlit web app
 st.title("Raag Prediction from Audio")
@@ -79,5 +57,5 @@ st.title("Raag Prediction from Audio")
 audio_file = st.file_uploader("Upload an audio file", type=["mp3", "wav", "ogg", "m4a", "flac"])
 
 if audio_file is not None:
-    predicted_raag = predict_raag(audio_file, model, label_encoder)
+    predicted_raag = predict_raag(audio_file, model, labels)
     st.write("Predicted Raag:", predicted_raag)
