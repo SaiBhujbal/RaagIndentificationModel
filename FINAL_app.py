0edd71d3eabfd8910a581a1a1462c00d2c66adde
Slayer <46700402+SaiBhujbal@users.noreply.github.com>
2023-10-30 04:02:50 +0530

    Update FINAL_app.py
    updated the entire streamlit code which will use labels_encoder.pkl for inverse transform....

--- FINAL_app.py
+++ FINAL_app.py
@@ -5,9 +5,13 @@ from pydub import AudioSegment
 from io import BytesIO
 from scipy import signal
 from tensorflow import keras
+import joblib
 
 # Load the trained model
-model = keras.models.load_model('mfcc_cnn_model.h5')  # Load the saved model
+model = keras.models.load_model('mfcc_cnn_model_2.h5')  # Load the saved model
+
+# Load the label encoder from the pickled file
+label_encoder = joblib.load('label_encoder.pkl')  # Replace 'label_encoder.pkl' with your label encoder filename
 
 # Function to extract MFCC features from an audio file
 def extract_mfcc(audio, sample_rate, num_mfcc=13, n_fft=2048, hop_length=512):
@@ -19,8 +23,7 @@ def extract_mfcc(audio, sample_rate, num_mfcc=13, n_fft=2048, hop_length=512):
 
     return mfccs
 
-def detect_and_convert_to_C_sharp(file_path):
-    audio = AudioSegment.from_file(file_path)
+def detect_and_convert_to_C_sharp(audio):
     sample_rate = audio.frame_rate
     y = np.array(audio.get_array_of_samples())
     f, t, S = signal.spectrogram(y, sample_rate)
@@ -36,24 +39,21 @@ def detect_and_convert_to_C_sharp(file_path):
     C_sharp_freq = 277.18  # Frequency of C# (assuming it's in Hz)
     freq_ratio = C_sharp_freq / note_freq
 
-    audio = audio._spawn(audio.raw_data, overrides={
+    converted_audio = audio._spawn(audio.raw_data, overrides={
         "frame_rate": int(audio.frame_rate * freq_ratio)
     })
 
     buffer = BytesIO()
-    audio.export(buffer, format="mp3")
+    converted_audio.export(buffer, format="wav")
     buffer.seek(0)
     return buffer
 
 # Function to predict raag from MFCC features
-def predict_raag(file_path, model):
-    buffer = detect_and_convert_to_C_sharp(file_path)
+def predict_raag(audio, model, label_encoder):
+    buffer = detect_and_convert_to_C_sharp(audio)
     audio, sample_rate = librosa.load(buffer, sr=None)
-
-# Function to predict raag from MFCC features
-def predict_raag(audio, model, sample_rate):
-    # Extract MFCC features from the audio
-    mfccs = extract_mfcc(audio, sample_rate)
+    # Extract MFCC features from the audio file
+    mfccs = extract_mfcc(audio, sample_rate)  # Extracting MFCCs from the audio variable
 
     # Reshape and normalize the MFCC data
     desired_shape = (216, 13)  # Shape expected by the model
@@ -71,8 +71,11 @@ def predict_raag(audio, model, sample_rate):
 
     # Get the predicted label
     predicted_label = np.argmax(predictions, axis=1)
+    
+    # Map the predicted label to raag names
+    predicted_raag = label_encoder.inverse_transform(predicted_label)
 
-    return predicted_label
+    return predicted_raag
 
 # Streamlit web app
 st.title("Raag Prediction from Audio")
@@ -81,9 +84,5 @@ audio_file = st.file_uploader("Upload an audio file", type=["mp3", "wav", "ogg",
 
 if audio_file is not None:
     audio = AudioSegment.from_file(audio_file, format=audio_file.name.split('.')[-1])
-    audio_bytes = audio.export(format='wav').read()  # Export the audio as WAV and read the bytes
-
-    audio, sample_rate = librosa.load(BytesIO(audio_bytes), sr=None)  # Load audio from bytes
-
-    predicted_raag = predict_raag(audio, model, sample_rate)
+    predicted_raag = predict_raag(audio, model, label_encoder)
     st.write("Predicted Raag:", predicted_raag)
